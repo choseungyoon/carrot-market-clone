c7e371151f7008baea30953b4c9605a5d2b3fc09
choseungyoon <cho31250@gmail.com>
2022-10-04 22:57:35 +0900

    Ready for backend

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,25 +1,27 @@
 import type { NextPage } from "next";
-import Layout from "../components/layout";
+import Link from "next/link";
+import FloatingButton from "../../components/floating-button";
+import Layout from "../../components/layout";
 
 const Community: NextPage = () => {
   return (
-    <Layout title="채팅" hasTabBar>
-      <div className="py-16 px-4 space-y-8">
-        {[1, 2, 3, 4, 5].map((_, i) => (
-          <div key={i}>
-            <div className="flex flex-col items-start cursor-pointer">
-              <span className="flex items-center px-2.5 py-0.5 rounded-full text-xs bg-gray-100 text-gray-800">
+    <Layout hasTabBar title="동네생활">
+      <div className="space-y-4 divide-y-[2px]">
+        {[1, 2, 3, 4, 5, 6].map((_, i) => (
+          <Link key={i} href={`/community/${i}`}>
+            <a className="flex cursor-pointer flex-col pt-4 items-start">
+              <span className="flex ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
                 동네질문
               </span>
-              <div className="mt-2 text-gray-700">
+              <div className="mt-2 px-4 text-gray-700">
                 <span className="text-orange-500 font-medium">Q.</span> What is
                 the best mandu restaurant?
               </div>
-              <div className="mt-5 flex items-center justify-between w-full text-gray-400 text-sm">
+              <div className="mt-5 px-4 flex items-center justify-between w-full text-gray-500 font-medium text-xs">
                 <span>니꼬</span>
                 <span>18시간 전</span>
               </div>
-              <div className="flex space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px] w-full">
+              <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t   w-full">
                 <span className="flex space-x-2 items-center text-sm">
                   <svg
                     className="w-4 h-4"
@@ -55,10 +57,10 @@ const Community: NextPage = () => {
                   <span>답변 1</span>
                 </span>
               </div>
-            </div>
-          </div>
+            </a>
+          </Link>
         ))}
-        <button className="fixed hover:bg-orange-500 cursor-pointer transition-colors bottom-24 right-5 rounded-full bg-orange-400 shadow-xl text-white p-4">
+        <FloatingButton href="/community/write">
           <svg
             className="w-6 h-6"
             fill="none"
@@ -73,7 +75,7 @@ const Community: NextPage = () => {
               d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
             ></path>
           </svg>
-        </button>
+        </FloatingButton>
       </div>
     </Layout>
   );
--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -1,29 +1,26 @@
 import type { NextPage } from "next";
-import Layout from "../components/layout";
+import Button from "../../components/button";
+import Layout from "../../components/layout";
 
 const ItemDetail: NextPage = () => {
   return (
     <Layout canGoBack>
-      <div className="px-4 py10">
+      <div className="px-4  py-4">
         <div className="mb-8">
           <div className="h-96 bg-slate-300" />
-
-          <div className="flex py-3 border-b items-center space-x-3">
+          <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
             <div className="w-12 h-12 rounded-full bg-slate-300" />
-
             <div>
-              <p className="text-sm font-medium text-gray-900">Steve Jebs</p>
-
-              <p className="text-xs font-medium">View profile &rarr;</p>
+              <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
+              <p className="text-xs font-medium text-gray-500">
+                View profile &rarr;
+              </p>
             </div>
           </div>
-
-          <div className="mt-8">
+          <div className="mt-5">
             <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
-
-            <span className="text-3xl block mt-3 text-gray-900">$140</span>
-
-            <p className="text-base my-6">
+            <span className="text-2xl block mt-3 text-gray-900">$140</span>
+            <p className=" my-6 text-gray-700">
               My money&apos;s in that office, right? If she start giving me some
               bullshit about it ain&apos;t there, and we got to go someplace
               else and get it, I&apos;m gonna shoot you in the head then and
@@ -33,13 +30,9 @@ const ItemDetail: NextPage = () => {
               in there, and that ni**a Winston or anybody else is in there, you
               the first motherfucker to get shot. You understand?
             </p>
-
             <div className="flex items-center justify-between space-x-2">
-              <button className="flex-1 bg-orange-500 text-white py-3 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 font-medium hover:bg-orange-600 shadow-md">
-                Talk to seller
-              </button>
-
-              <button className="p-3 flex justify-center text-gray-400 hover:bg-gray-400">
+              <Button large text="Talk to seller" />
+              <button className="p-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500">
                 <svg
                   className="h-6 w-6 "
                   xmlns="http://www.w3.org/2000/svg"
@@ -59,18 +52,14 @@ const ItemDetail: NextPage = () => {
             </div>
           </div>
         </div>
-
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
-
-          <div className="mt-6 grid grid-cols-2 gap-4">
+          <div className=" mt-6 grid grid-cols-2 gap-4">
             {[1, 2, 3, 4, 5, 6].map((_, i) => (
               <div key={i}>
                 <div className="h-56 w-full mb-4 bg-slate-300" />
-
                 <h3 className="text-gray-700 -mb-1">Galaxy S60</h3>
-
-                <p className="text-sm text-gray-900 font-medium">$6</p>
+                <span className="text-sm font-medium text-gray-900">$6</span>
               </div>
             ))}
           </div>
