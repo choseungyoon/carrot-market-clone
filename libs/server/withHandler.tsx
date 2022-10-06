6e16e3ce42a890771e58d471defc42a96b8fc8f6
choseungyoon <cho31250@gmail.com>
2022-10-06 22:57:14 +0900

    Done create account api with prisma

--- libs/server/withHandler.tsx
+++ libs/server/withHandler.tsx
@@ -5,6 +5,14 @@ export default function withHandler(
   fn: (req: NextApiRequest, res: NextApiResponse) => void
 ) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
-    res.json({ hello: true });
+    if (req.method !== method) {
+      return res.status(405).end();
+    }
+    try {
+      await fn(req, res);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error });
+    }
   };
 }
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,21 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  let user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+
+  console.log(user);
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
